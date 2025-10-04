c1f107853f71f41c59f6cffa9cafe2a52b7a6704
Théo <to.constant@gmail.com>
2025-10-05 00:14:39 +0200

    Respawn objects in scene when restarting a game without reloading the scene

---
+++ Assets/Scripts/SpawnerOnRestart.cs
@@ -0,0 +1,60 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerOnRestart : MonoBehaviour
+{
+    public Transform Prefab;
+
+    public List<SceneObjectInfo> ObjectsFromScene = new();
+    public bool GameHasBeenStartedOnce;
+
+    private void Awake()
+    {
+        ObjectsFromScene.Clear();
+
+        foreach (Rigidbody2D sceneObject in transform.GetComponentsInChildren<Rigidbody2D>())
+        {
+            ObjectsFromScene.Add(new(sceneObject.transform));
+        }
+
+        GameManager.Instance.OnGameStarted += GameManager_OnGameStarted;
+    }
+
+    private void GameManager_OnGameStarted()
+    {
+        if (GameHasBeenStartedOnce)
+        {
+            RespawnObjects();
+        }
+
+        GameHasBeenStartedOnce = true;
+    }
+
+    public void RespawnObjects()
+    {
+        foreach (Transform child in transform)
+        {
+            Destroy(child.gameObject);
+        }
+
+        foreach (SceneObjectInfo sceneObjectInfo in ObjectsFromScene)
+        {
+            Transform respawnedObject = Instantiate(Prefab, sceneObjectInfo.Position, sceneObjectInfo.Rotation, transform);
+            respawnedObject.localScale = sceneObjectInfo.Scale;
+        }
+    }
+
+    public struct SceneObjectInfo
+    {
+        public Vector3 Position;
+        public Quaternion Rotation;
+        public Vector3 Scale;
+
+        public SceneObjectInfo(Transform sceneObject)
+        {
+            Position = sceneObject.position;
+            Rotation = sceneObject.rotation;
+            Scale = sceneObject.localScale;
+        }
+    }
+}
